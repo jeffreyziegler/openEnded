27ccba5172ed8fb7abe4a7bac98b569fbcacccbe
jeffreyziegler <zieglerjef@gmail.com>
2020-03-10 22:20:27 -0400

    Update function descriptions

--- R/plotSimilarity.R
+++ R/plotSimilarity.R
@@ -1,27 +1,25 @@
 #' @title plotSimilarity
 #' 
-#' @description
-#' Plot similarity measure
-#' 
-#' @details 
-#' 
-#' This function plots the raw distribution for a given similarity measure
+#' @description 
+#' This function plots the raw distribution for a given similarity measure.
 #'
+#' @param dataframe Dataframe that contains the similarity measures you want to plot.
+#' @param measure Vector in dataframe that contains the similarity measures you want to plot.
+
 #'
 #' @author Jeffrey Ziegler
 #' @examples
 #' 
-#' \dontrun{
-#' plotSimilarity(dataframe=kaneData, method="jaccard")
-#' }
+#' plotSimilarity(dataframe=replication_complete.cases, measure="jaccardDist")
+#' 
 #' @rdname plot_similarity
 #' @seealso \code{\link{similarity_measures}}
 #' @export
 
-plot_similarity <- function(dataframe, method="jaccard"){
-  ggplot(dataframe, aes(x = jaccardDist)) +  
+plotSimilarity <- function(dataframe, measure){
+  ggplot(dataframe, aes(x = measure)) +  
     geom_histogram(binwidth=.025, colour="black", fill="white") + lims(x=c(0,1), y=c(0,150))+
-    labs(x=paste(method, "Similarity", sep=" "), y="Number of Respondents")+ theme_classic() +
+    labs(x=paste(measure, "Similarity", sep=" "), y="Number of Respondents")+ theme_classic() +
     geom_vline(aes(xintercept=mean(jaccardDist, na.rm=T)),   # Ignore NA values for mean
                color="black", linetype="dashed", size=1.5) +
     theme(legend.position="none", axis.title = element_text(size=30), axis.text = element_text(size=25))  
--- R/similarityMeasures.R
+++ R/similarityMeasures.R
@@ -1,23 +1,21 @@
 #' @title simlarityMeasures
-#' @details 
+#' @description 
 #' Calculate measure of similarity between prompt and response in open-ended manipulation check.
 #' 
 #' @param dataframe Dataframe that contains the prompt and response of the open-ended manipulation check.
 #' @param prompt Vector/column within `dataframe` that contains the prompt.
 #' @param response Vector/column within `dataframe` that contains the open-ended response.
-#' @param measure_type . Possible values for measure_type = c("osa", "lv", "dl", "hamming", "lcs", "qgram", "cosine", "jaccard", "jw", "soundex"). Default is "jaccard".
+#' @param measure_type Possible values for measure_type = c("osa", "lv", "dl", "hamming", "lcs", "qgram", "cosine", "jaccard", "jw", "soundex"). Default is "jaccard".
 #' @param ngrams The number of grams/segments words should be broken into. Default n=3.
 #'
 #' @return Vector of similarity measures (same length as input dataframe)
 #'
 #' @author Jeffrey Ziegler
 #' @examples
-#' \dontrun{
 #' simlarityMeasures(replication_complete.cases, prompt="textViewed", response="validityCheck", ngrams=3)
-#' }
 #' @rdname simlarityMeasures
 #' @export
 
-similarity_measures <- function(dataframe, prompt, response, measure_type="jaccard", ngrams=3){
+similarityMeasures <- function(dataframe, prompt, response, measure_type="jaccard", ngrams=3){
   1-stringdist(enc2utf8(dataframe[, prompt]), enc2utf8(dataframe[, response]), useBytes=TRUE,  method=measure_type, q=ngrams)
 }
