de29169f0893641998dca3b0cc83f2a11d87b976
“zieglerjef” <zieglerjef@gmail.com>
2020-08-24 15:28:16 -0400

    Add complierATE.R and associated code

--- R/ATEcutoff.R
+++ R/ATEcutoff.R
@@ -20,21 +20,114 @@
 #' @rdname ATEcutoff
 #' @export
 
-ATEcutoff <- function(formula, n_sims, similarity_measures, model_type, k=3, bounds=c(0.1, 0.2)){
-    cutoff <- 1-runif(1, min=bounds[1], max=bounds[2])
+ATEcutoff <- function(dataframe, 
+                      formula, 
+                      similarity_measures, 
+                      model_type, 
+                      k, 
+                      bounds, 
+                      user_seed,
+                      plot_treatment=NULL,
+                      plot_interact_x=NULL, 
+                      stable_x){
+    #browser()
+    cutoff <- runif(1, min=bounds[1], max=bounds[2])
     dataframe <- averageSimilarity(dataframe, similarity_measures,  k)
-    dataframe$passFail <- ifelse(dataframe[, "avgSimilarity"] >= cutoff, 0, 1)
-
-    passReg <- zelig(formula, data=dataframe[which(dataframe$passFail==1),], model=model_type, weights="avgSimilarity", cite=F)
-    failReg <- zelig(formula, data=dataframe[which(dataframe$passFail==0),],  model=model_type, weights="avgSimilarity", cite=F)
+    dataframe$passFail <- ifelse(dataframe[, "avgSimilarity"] <= cutoff, 0, 1)
     
-    pass1stDiffPlotData <- NULL
-    pass1stDiffPlotData <- rbind(generateMarginalEffect(temp_model = passReg),
-                                 generateMarginalEffect(temp_model = failReg))
+    # estimate models
+    if(model_type=="ols"){
+        passReg <- lm(formula, data=dataframe[which(dataframe$passFail==1),])
+        failReg <- lm(formula, data=dataframe[which(dataframe$passFail==0),])
+        
+    }
+    # currently, only support logit aside from OLS
+    if(model_type=="logit"){
+        passReg <- glm(formula, data=dataframe[which(dataframe$passFail==1),], family=binomial(link="logit"))
+        failReg <- glm(formula, data=dataframe[which(dataframe$passFail==0),], family=binomial(link="logit"))
+    }
     
-    pass1stDiffPlotData$subset <- c(rep("pass", n_sims), rep("fail", n_sims))
+    pass1stDiffPlotData <- list()
+    temp_models <- list(passReg, failReg)
+    for(sample in 1:length(temp_models)){
+        # create model matrix of dummies from specified formula
+        
+        full_dummies <- model.matrix(model.frame(formula, data=temp_models[[sample]]$data),
+                                     data=temp_models[[sample]]$data)
+        # get unique combos for sims of marginal effects
+        unique_dummies <- unique(full_dummies)
+        # point estimates from regression
+        point_estimates <- temp_models[[sample]]$coefficients 
+        # var-cov matrix
+        var_cov_mat <- vcov(temp_models[[sample]])
+        # simulate parameter distributions
+        sim_betas <- as.matrix(mvrnorm(10000, point_estimates, var_cov_mat))
+       
+        fd_labs <- unique(model.frame(formula, data=temp_models[[sample]]$data)[-1]) %>% unite("combo", sep=":", na.rm = T, remove = F)
+        # plot simulated first diffs
+        pass1stDiffPlotData[[sample]] <- generateMarginalEffect(unique_covars = unique_dummies, 
+                                                                simulated_betas=sim_betas, 
+                                                                diff_labs=fd_labs[,1],
+                                                                raw=F)
+        #browser()
+        
+        #pass1stDiffPlotData[[sample]] <- reshape2::melt(pass1stDiffPlotData[[sample]])
+        #grep(paste(toMatch,collapse="|"), 
+        #     myfile$Letter, value=TRUE)
+        pass1stDiffPlotData[[sample]]$treat_from <- sapply(str_match_all(pass1stDiffPlotData[[sample]]$covar_cats, 
+                                                                         paste(unique(temp_models[[sample]]$data[, plot_treatment]),
+                                                                               collapse = "|")), "[", 1)
+        pass1stDiffPlotData[[sample]]$treat_to <- sapply(str_match_all(pass1stDiffPlotData[[sample]]$covar_cats, 
+                                                                       paste(unique(temp_models[[sample]]$data[, plot_treatment]),
+                                                                             collapse = "|")), "[", 2)
+        
+        pass1stDiffPlotData[[sample]]$treat_from_to <- as.factor(paste(pass1stDiffPlotData[[sample]]$treat_from,
+                                                                       pass1stDiffPlotData[[sample]]$treat_to,
+                                                                       sep=" to "))
+        
+        pass1stDiffPlotData[[sample]]$interact_x_from <- sapply(str_match_all(pass1stDiffPlotData[[sample]]$covar_cats, 
+                                                                              paste(temp_models[[sample]]$xlevels[[plot_interact_x]],
+                                                                                    collapse = "|")), "[", 1) 
+        pass1stDiffPlotData[[sample]]$interact_x_to <- sapply(str_match_all(pass1stDiffPlotData[[sample]]$covar_cats, 
+                                                                            paste(temp_models[[sample]]$xlevels[[plot_interact_x]],
+                                                                                  collapse = "|")), "[", 2) 
+        if(stable_x==T){
+            pass1stDiffPlotData[[sample]] <- pass1stDiffPlotData[[sample]][which(pass1stDiffPlotData[[sample]]$interact_x_from==pass1stDiffPlotData[[sample]]$interact_x_to),]
+            pass1stDiffPlotData[[sample]]$interact_x <- as.factor(pass1stDiffPlotData[[sample]]$interact_x_from)
+            pass1stDiffPlotData[[sample]] <- pass1stDiffPlotData[[sample]][, c("first_diffs", "treat_from_to", "interact_x")]
+        }
+        
+    }
+    pass1stDiffPlotData <- bind_rows(pass1stDiffPlotData, .id = "column_label")
     
-    pass1stDiffPlotData$outcome <- str_to_title(gsub("_.*","", gsub('([[:upper:]])', ' \\1', gsub("~.*","", formula)[2])))
-    pass1stDiffPlotData$cutoff <- cutoff
-    return(pass1stDiffPlotData) 
+    pass1stDiffPlotData$subset <- c(rep("Compliers", dim(pass1stDiffPlotData)[1]/2), rep("Non-Compliers",  dim(pass1stDiffPlotData)[1]/2))
+    normal_data <- pass1stDiffPlotData %>% group_by(treat_from_to, interact_x) %>% summarise(n = n(), .groups = 'drop') %>% filter(n>10000)
+    weird_data <- pass1stDiffPlotData %>% group_by(treat_from_to, interact_x) %>% summarise(n = n(), .groups = 'drop') %>% filter(n==10000)
+    
+    for(scenario in 1:dim(pass1stDiffPlotData)[1]){
+        # might need to do one for when k=1 and k=10
+        # or when user_seed=1, 10, or 100, for some reason
+        # the plots create an extra group
+       #browser()
+        
+        if(do.call(paste0, pass1stDiffPlotData[scenario, c("treat_from_to", "interact_x")])%in% do.call(paste0, weird_data[, c("treat_from_to", "interact_x")])){
+            pass1stDiffPlotData[scenario, "first_diffs"] <- -1*pass1stDiffPlotData[scenario, "first_diffs"]
+            pass1stDiffPlotData[scenario, "treat_from_to"] <- paste(rev(strsplit(as.character(pass1stDiffPlotData[scenario, "treat_from_to"]), "\\s+")[[1]]), collapse= " ")
+        }
+        
+        if(str_detect(pass1stDiffPlotData[scenario, c("treat_from_to")], "Control") & str_locate(pass1stDiffPlotData[scenario, c("treat_from_to")], "Control")[1]!=1){
+            pass1stDiffPlotData[scenario, "first_diffs"] <- -1*pass1stDiffPlotData[scenario, "first_diffs"]
+            pass1stDiffPlotData[scenario, "treat_from_to"] <- paste(rev(strsplit(as.character(pass1stDiffPlotData[scenario, "treat_from_to"]), "\\s+")[[1]]), collapse= " ")
+        }
+        
+        if(str_detect(pass1stDiffPlotData[scenario, c("treat_from_to")], "Appease to Alienate")){
+            pass1stDiffPlotData[scenario, "first_diffs"] <- -1*pass1stDiffPlotData[scenario, "first_diffs"]
+            pass1stDiffPlotData[scenario, "treat_from_to"] <- paste(rev(strsplit(as.character(pass1stDiffPlotData[scenario, "treat_from_to"]), "\\s+")[[1]]), collapse= " ")
+        }
+    }
+        #pass1stDiffPlotData$outcome <- str_to_title(gsub("_.*","", gsub('([[:upper:]])', ' \\1', gsub("~.*","", formula)[2])))
+        pass1stDiffPlotData$cutoff <- cutoff
+        #browser()
+        
+        return(pass1stDiffPlotData) 
 }
--- R/complierATE.R
+++ R/complierATE.R
@@ -18,60 +18,96 @@
 #' @rdname plotComplierATE
 #' @export
 
-complierATE <- function(models=NULL, 
+complierATE <- function(dataframe=NULL, 
+                        formula=NULL, 
+                        plot_treatment=NULL,
+                        plot_interact_x=NULL,
+                        similarity_measures=c("jaccardDist", "cosineDist"),
                         bounds=c(0.1, 0.2), 
                         n=100, 
                         user_seed=5, 
                         model_type=NULL, 
                         k=3,
-                        plot_path=NULL){
+                        display_plot=T,
+                        plot_path=NULL,
+                        stable_x){
   
   bootstrappedData <- data.frame()
-  for(model in 1:length(models)){
+  set.seed(user_seed)
     for(i in 1:n){
-      browser()
-      
       bootstrappedData <- rbind(bootstrappedData, ATEcutoff(dataframe, 
-                                                            similarity_measures=c("jaccardDist", "cosineDist"), 
-                                                            formula=models[[model]], model_type, k, bounds))
+                                                            formula,
+                                                            similarity_measures, 
+                                                            model_type,
+                                                            k, 
+                                                            bounds,
+                                                            user_seed,
+                                                            plot_treatment,
+                                                            plot_interact_x, 
+                                                            stable_x))
     }
-  }
-  
-  outputMatMeltedData <- reshape2::melt(bootstrappedData, id = c("subset", "cutoff", "outcome"))
-  outputQuartilesPlotData <- cbind(ddply(outputMatMeltedData, .(variable, cutoff, outcome, subset), summarize, FDmedian = median(value)),
-                                   FDlow90 = ddply(outputMatMeltedData, .(variable, cutoff, outcome, subset), summarize, FDlow = quantile(value, .05))[,5],
-                                   FDhigh90 = ddply(outputMatMeltedData, .(variable, cutoff, outcome, subset), summarize, FDhigh = quantile(value, .95))[,5],
-                                   FDlow95 = ddply(outputMatMeltedData, .(variable, cutoff, outcome, subset), summarize, FDlow = quantile(value, .025))[,5],
-                                   FDhigh95 = ddply(outputMatMeltedData, .(variable, cutoff, outcome, subset), summarize, FDhigh = quantile(value, .975))[, 5]
-  )
-  
-  outputQuartilesPlotData$marginalEffect <- gsub(".*\\(","", gsub("\\)","", outputQuartilesPlotData$variable))
-  outputQuartilesPlotData$attendance <- trimws(gsub("\\(.*","", outputQuartilesPlotData$variable), which="both")
-  outputQuartilesPlotData$subset <- relevel(revalue(as.factor(outputQuartilesPlotData$subset), replace = c("fail"="Non-compliers (Fail)","pass"="Compliers (Pass)")), ref = "Compliers (Pass)")
-  outputQuartilesPlotData$attendance <- factor(outputQuartilesPlotData$attendance, 
-                                               levels = c("Never/yearly","Monthly", "Weekly"))
-  
-  finalPlotData <- cbind(ddply(outputQuartilesPlotData, .(attendance, outcome, subset), summarize, FDmedian = median(FDmedian)),
-                                   FDlow90 = ddply(outputQuartilesPlotData, .(attendance, outcome, subset), summarize, FDlow = quantile(FDmedian, .05))[,4],
-                                   FDhigh90 = ddply(outputQuartilesPlotData, .(attendance, outcome,  subset), summarize, FDhigh = quantile(FDmedian, .95))[,4],
-                                   FDlow95 = ddply(outputQuartilesPlotData, .(attendance, outcome, subset), summarize, FDlow = quantile(FDmedian, .025))[,4],
-                                   FDhigh95 = ddply(outputQuartilesPlotData, .(attendance, outcome, subset), summarize, FDhigh = quantile(FDmedian, .975))[,4]
-  )
-  ggplot(finalPlotData, aes(x=attendance, y=FDmedian, colour=subset)) + theme_pubr() +
-    scale_shape_manual(values = c(17,18,19, 16))+
-    geom_hline(aes(yintercept= 0), linetype="dashed", size=.5, colour="black") +
-    geom_pointrange(aes(x=attendance, y  = FDmedian, ymin = FDlow95, ymax = FDhigh95, colour=subset, shape=subset),
-                    position = position_dodge(width =.75), size=1.5)+
-    facet_wrap(~outcome, ncol=3) + 
-    scale_colour_grey(start = 0, end = 0.7)+
+
+  # outputMatMeltedData <- reshape2::melt(bootstrappedData, id = c("subset", "cutoff", "outcome"))
+  # outputQuartilesPlotData <- cbind(ddply(outputMatMeltedData, .(variable, cutoff, outcome, subset), summarize, FDmedian = median(value)),
+  #                                  FDlow90 = ddply(outputMatMeltedData, .(variable, cutoff, outcome, subset), summarize, FDlow = quantile(value, .05))[,5],
+  #                                  FDhigh90 = ddply(outputMatMeltedData, .(variable, cutoff, outcome, subset), summarize, FDhigh = quantile(value, .95))[,5],
+  #                                  FDlow95 = ddply(outputMatMeltedData, .(variable, cutoff, outcome, subset), summarize, FDlow = quantile(value, .025))[,5],
+  #                                  FDhigh95 = ddply(outputMatMeltedData, .(variable, cutoff, outcome, subset), summarize, FDhigh = quantile(value, .975))[, 5]
+  # )
+  # 
+  # outputQuartilesPlotData$marginalEffect <- gsub(".*\\(","", gsub("\\)","", outputQuartilesPlotData$variable))
+  # outputQuartilesPlotData$attendance <- trimws(gsub("\\(.*","", outputQuartilesPlotData$variable), which="both")
+  # outputQuartilesPlotData$subset <- relevel(revalue(as.factor(outputQuartilesPlotData$subset), replace = c("fail"="Non-compliers (Fail)","pass"="Compliers (Pass)")), ref = "Compliers (Pass)")
+  # outputQuartilesPlotData$attendance <- factor(outputQuartilesPlotData$attendance, 
+  #                                              levels = c("Never/yearly","Monthly", "Weekly"))
+  # 
+  # finalPlotData <- cbind(ddply(outputQuartilesPlotData, .(attendance, outcome, subset), summarize, FDmedian = median(FDmedian)),
+  #                                  FDlow90 = ddply(outputQuartilesPlotData, .(attendance, outcome, subset), summarize, FDlow = quantile(FDmedian, .05))[,4],
+  #                                  FDhigh90 = ddply(outputQuartilesPlotData, .(attendance, outcome,  subset), summarize, FDhigh = quantile(FDmedian, .95))[,4],
+  #                                  FDlow95 = ddply(outputQuartilesPlotData, .(attendance, outcome, subset), summarize, FDlow = quantile(FDmedian, .025))[,4],
+  #                                  FDhigh95 = ddply(outputQuartilesPlotData, .(attendance, outcome, subset), summarize, FDhigh = quantile(FDmedian, .975))[,4]
+  # )
+  p1 <- ggplot(bootstrappedData, aes(x=first_diffs, y=as.factor(treat_from_to), colour=subset, fill=subset)) +
+    #theme_classic() +
+    theme_pubr() +
+    
+    #scale_shape_manual(values = c(17,18,19, 16))+
+    geom_vline(aes(xintercept=0), linetype="dashed", size=.5, colour="black") +
+    
+    #geom_split_violin()+
+    
+   # geom_violin(position=position_dodge(1)) + stat_summary(fun.data=data_summary, position=position_dodge(1)) +
+    #ggplot(weather_atl, aes(x = windSpeed, y = fct_rev(Month), fill = Month)) +
+    geom_density_ridges(quantile_lines = F,  alpha=.75) +
+    #geom_beeswarm(priority='density',cex=2.5) +
+    #geom_pointrange(aes(x=treat_from_to, y  = first_diffs, ymin = lower_CI, ymax = upper_CI, colour=subset, shape=subset),
+    #               position = position_dodge(width =.75), size=1.5)+
+    #coord_flip()+
+    
+    facet_wrap(~interact_x, ncol=3, scales="fixed") + 
+    scale_colour_grey(start = 0.2, end = 0.7)+
+    scale_fill_grey(start = 0.2, end = 0.7)+
+    lims(x=c(min(bootstrappedData$first_diffs)-0.1, max(bootstrappedData$first_diffs)+0.1))+
     theme(axis.title=element_text(size=20), axis.text = element_text(size=18), legend.text=element_text(size=18),
           strip.text = element_text(size=20), strip.background = element_rect(fill = NA, color = "black"),
           legend.position="bottom", legend.title = element_text(size=20),
           title = element_text(size=25),  legend.background = element_blank(),
-          legend.box.background = element_rect(colour = "black"), axis.text.x = element_text(angle = 90, hjust = 1),
+          legend.box.background = element_rect(colour = "black"), axis.text.x = element_text(angle = 45, hjust = 1),
           panel.border = element_blank(), 
           panel.background = element_rect(fill = NA, color = "black"),
-          panel.grid = element_blank()) + 
+          panel.grid = element_blank()
+          #  strip.background = element_blank(),
+          # strip.text.x = element_blank()
+    ) + 
     geom_vline(xintercept = c(1.5,2.5)) +
-    labs(y='\nMarginal Effect of Treatment\n', x='\nTreatment\n', colour="Sample:", shape="Sample:")
+    labs(x='\nMarginal Effect of Treatment\n', y='\nTreatment Condition\n', colour="Sample:", fill="Sample:"
+    )
+  if(display_plot==T){
+    print(p1)
+  }
+  if(!is.null(plot_path)){
+    pdf(plot_path)
+    print(p1)
+    dev.off()
+  }
 }
